6c92d459ba578768567bfa9c8619b75144b4c373
Pedro Pinero <penriques1985@gmail.com>
2022-02-19 10:37:11 -0500

    shortcode swiper 1.3 Images and design changed

--- wp-content/themes/twentytwentyone/custom_shortcode.php
+++ wp-content/themes/twentytwentyone/custom_shortcode.php
@@ -3,54 +3,56 @@
 
 function test_swiper($atts)
 {
-  $params = shortcode_atts(array(
-    'id' => '',
-    'slides_per_view' => 1,
-    'pagination' => false
-  ), $atts);
+    $params = shortcode_atts(array(
+        'id' => '',
+        'slides_per_view' => 1,
+        'pagination' => false
+    ), $atts);
 
-  ob_start();
-  ?>
-  <div class="swiper mySwiper<?php echo '-'.$params['id'] ?>">
-    <div class="swiper-wrapper">
-      <div class="swiper-slide"><img src="https://via.placeholder.com/300" alt=""></div>
-      <div class="swiper-slide"><img src="https://via.placeholder.com/300" alt=""></div>
-      <div class="swiper-slide"><img src="https://via.placeholder.com/300" alt=""></div>
-      <div class="swiper-slide"><img src="https://via.placeholder.com/300" alt=""></div>
-      <div class="swiper-slide"><img src="https://via.placeholder.com/300" alt=""></div>
-      <div class="swiper-slide"><img src="https://via.placeholder.com/300" alt=""></div>
-      <div class="swiper-slide"><img src="https://via.placeholder.com/300" alt=""></div>
-      <div class="swiper-slide"><img src="https://via.placeholder.com/300" alt=""></div>
-      <div class="swiper-slide"><img src="https://via.placeholder.com/300" alt=""></div>
+    ob_start();
+?>
+    <div class="swiper mySwiper<?php echo '-'.$params['id'] ?>">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide"><img src="http://placekitten.com/300" alt=""></div>
+            <div class="swiper-slide"><img src="https://placekitten.com/g/300" alt=""></div>
+            <div class="swiper-slide"><img src="http://placekitten.com/300" alt=""></div>
+            <div class="swiper-slide"><img src="https://placekitten.com/g/300" alt=""></div>
+            <div class="swiper-slide"><img src="http://placekitten.com/300" alt=""></div>
+            <div class="swiper-slide"><img src="https://placekitten.com/g/300" alt=""></div>
+            <div class="swiper-slide"><img src="http://placekitten.com/300" alt=""></div>
+            <div class="swiper-slide"><img src="https://placekitten.com/g/300" alt=""></div>
+        </div>
+
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+
+        <?php if ($params['pagination'] == 'true'): ?>
+            <div class="swiper-pagination"></div>
+        <?php endif; ?>
     </div>
-    <div class="swiper-button-next"></div>
-    <div class="swiper-button-prev"></div>
-    <?php if ($params['pagination'] == 'true'): ?>
-    <div class="swiper-pagination"></div>
-    <?php endif; ?>
-  </div>
 
-  <script>
-    new Swiper(".mySwiper<?php echo '-'.$params['id'] ?>", {
-      slidesPerView: <?php echo $params['slides_per_view']; ?>,
-      <?php if ($params['pagination'] == 'true'): ?>
-      pagination: {
-        el: ".swiper-pagination",
-      },
-      <?php endif; ?>
-      spaceBetween: 30,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
-  </script>
+    <script>
+        new Swiper(".mySwiper<?php echo '-'.$params['id'] ?>", {
+            slidesPerView: <?php echo $params['slides_per_view']; ?>,
 
-  <?php
-  $html = ob_get_clean();
+            <?php if ($params['pagination'] == 'true'): ?>
+                pagination: {
+                    el: ".swiper-pagination",
+                },
+            <?php endif; ?>
 
-  return $html;
+            spaceBetween: 0,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
+    </script>
 
+    <?php
+    $html = ob_get_clean();
+
+    return $html;
 }
 
 add_shortcode('custom_swiper', 'test_swiper');
